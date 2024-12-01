1997e3fd723b40b7aefbdb47bc2e43cf1f5c1405
Sebastian Gnip <sebastian.gnip@baslerweb.com>
2024-12-01 16:22:33 +0100

    Day 1 Part 2 done

--- day1.py
+++ day1.py
@@ -1,15 +1,28 @@
-
+# Part1
 list1 = []
 list2 = []
 
 with open("day1_input.md", "r", encoding="utf-8") as file:
     for line in file:
         left, right = line.strip().split("   ")
-        list1.append(left)
-        list2.append(right)
+        list1.append(int(left))
+        list2.append(int(right))
 
 list1.sort()
 list2.sort()
 
-distance = [abs(int(x) - int(y)) for x, y in zip(list1, list2)]
-print(sum(distance))
+distance = sum([abs((x)- (y)) for x, y in zip(list1, list2)])
+print(distance)
+
+# Part2
+result = 0
+
+for x in list1:
+    i = 0
+    for y in list2:
+        if x == y:
+            i += 1
+    print(x, i, x * i)
+    result = result + x * i
+
+print(result)
