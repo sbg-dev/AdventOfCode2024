36fb6b09fb9d584741961413705fe88841cf78a8
Sebastian Gnip <sebastian.gnip@baslerweb.com>
2024-12-03 10:31:17 +0100

    Day 3 Part 1 done

--- day3.py
+++ day3.py
@@ -1,16 +1,19 @@
 from typing import List
 import re
-# Part 1
+
+# Part 1 = 167090022
 data: List = []
-regex = re.compile("mul\((\d{1,3}),(\d{1,3})\)")
-with open("day3_test_input.md", "r", encoding="utf-8") as f:
+regex = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+with open("day3_input.md", "r", encoding="utf-8") as f:
     for line in f:
         data.append(line.strip())
-print(data)
-for line in data:
-    instructions = regex.findall(line)
-print(instructions)
-sum: int = 0
-for factor1, factor2 in instructions:
-    sum += int(factor1) * int(factor2)
-print(sum)
+
+instructions: List = []
+instructions = [regex.findall(i) for i in data]
+
+total: int = 0
+for line in instructions:
+    for factor1, factor2 in line:
+        total += int(factor1) * int(factor2)
+print(total)
