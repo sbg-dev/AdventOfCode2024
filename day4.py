1badec20ca5ea4a4004cbc66f0c87240066a39d5
Sebastian Gnip <sebastian.gnip@baslerweb.com>
2024-12-06 09:49:10 +0100

    Refactor day4

--- day4.py
+++ day4.py
@@ -2,25 +2,21 @@ from typing import List
 
 # Day 4 Part 1 = 2583
 data: List = []
-with open("day4_input.md", "r", encoding="utf-8") as f:
+with open("day4_test_input.md", "r", encoding="utf-8") as f:
     for line in f:
         data.append(line.strip())
 
 coordinates = [(0,1), (0,-1), (-1,0), (1,0), # horizontal and vertical
                 (-1,1), (1,1), (1,-1), (-1,-1)] # diagonal clockwise rotation
+word = "XMAS"
 
-def search_directions(r, c, dr, dc):
-    for i in range(len("XMAS")):
+def search_directions(r, c, dr, dc, word):
+    for i in range(len(word)):
         nr = r + dr * i
         nc = c + dc * i
-        if not 0 <= nc < column_count:
-            print("Column out of bounds")
+        if not (0 <= nc < column_count and 0 <= nr < row_length):
             return False
-        if not 0 <= nr < row_length:
-            print("Row out of bounds")
-            return False
-        print(data[nr][nc])
-        if data[nr][nc] != 'XMAS'[i]:
+        if data[nr][nc] != word[i]:
             return False
     return True
 
@@ -33,6 +29,6 @@ for row in range(row_length):
     print(f"Column: {data[row]}")
     for column in range(column_count):
         for dr, dc in coordinates:
-            if search_directions(row, column, dr, dc):
+            if search_directions(row, column, dr, dc, word):
                 count += 1
 print(count)
